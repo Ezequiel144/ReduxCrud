a423a4ecd879d8e65517b6a69e1729d2eac35728
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-07-31 13:08:23 -0300

    taskDetails terminado,modificacion leve del narbar,hacer el redux

--- src/components/Data/DataList.js
+++ src/components/Data/DataList.js
@@ -1,15 +1,33 @@
+import imgPrueba from "../../asset/imagen1.jpg";
+import imgPrueba2 from "../../asset/girl3.jpg";
 const taskList = [
     {
         id: 1,
-        title:'titulos_1',
-        descrip:'text text text text text 1',
+        title:'trabajo',
+        descrip:'organizar horarios',
         textLarge:'textolargo textolargo textolargo',
+        img: imgPrueba
     },
     {
         id: 2,
-        title:'titulos_2',
-        descrip:'text text text text text 2',
+        title:'tarea',
+        descrip:'hacer tp de geografia',
         textLarge:'textolargo textolargo textolargo',
+        img: imgPrueba
+    },
+    {
+        id: 3,
+        title:'compras',
+        descrip:'te,frutas,carne,leche',
+        textLarge:'textolargo textolargo textolargo',
+        img: imgPrueba2
+    },
+    {
+        id: 4,
+        title:'facturas',
+        descrip:'lux,gas,agua,...',
+        textLarge:'textolargo textolargo textolargo',
+        img: imgPrueba2
     }
 ]
 
--- src/views/PagesTaskDetails/PagesTaskDetails.jsx
+++ src/views/PagesTaskDetails/PagesTaskDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import taskList from "../../components/Data/DataList.js";
+import styleDetails from "./PagestaskDetails.module.css"
 
 export default function PagesTaskDetails(){
     const {idtask} = useParams();
@@ -8,15 +9,38 @@ export default function PagesTaskDetails(){
     const [task,setTask] = useState(taskList);
 
     useEffect(()=>{
-        setTask(task.find(t => t.id === idtask))
-        console.log("ID Tarea: ", idtask)
+        setTask(task.find(t => t.id === parseInt(idtask)))
+        /* console.log("ID Tarea: ", idtask) */
     },[idtask]);
 
-    console.log(task);
+    
 
     return(
-        <>
-            <p>destalles del id {idtask}</p>
-        </>
+        <section className={styleDetails.contentDetails}>
+            <Details 
+                title={task.title}
+                descrip={task.descrip}
+                textLarge={task.textLarge}
+                img={task.img}
+            />
+        </section>
+    )
+}
+
+function Details({title,descrip,textLarge,img}){
+    return(
+        <section>
+            <article className={styleDetails.contentTitleDescrip}>
+                <h1 className={styleDetails.title}>{title}</h1>
+                <p className={styleDetails.descrip}>{descrip}</p>
+            </article>
+            <article className={styleDetails.contentTextImg}>
+                <section className={styleDetails.sizeImg}>
+                    <img className={styleDetails.imagen} src={img} alt={img} />
+                </section>
+                <p className={styleDetails.textLarge}>{textLarge}</p>
+            </article>
+
+        </section>
     )
 }
